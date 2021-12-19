5e00dd449e89ac0c5392fb1788b8f09a1a7e394c
alicja-ogonowska <kontakt@alicjaogonowska.pl>
2021-12-19 12:09:56 +0100

    Update OkHttp settings for Android

--- android/src/main/java/com/bluechilli/flutteruploader/UploadWorker.java
+++ android/src/main/java/com/bluechilli/flutteruploader/UploadWorker.java
@@ -40,6 +40,13 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
+import java.security.cert.CertificateException;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 public class UploadWorker extends ListenableWorker implements CountProgressListener {
   public static final String ARG_URL = "url";
@@ -232,12 +239,12 @@ public class UploadWorker extends ListenableWorker implements CountProgressListe
 
       Log.d(TAG, "Start uploading for " + tag);
 
-      OkHttpClient client =
-          new OkHttpClient.Builder()
-              .connectTimeout((long) timeout, TimeUnit.SECONDS)
-              .writeTimeout((long) timeout, TimeUnit.SECONDS)
-              .readTimeout((long) timeout, TimeUnit.SECONDS)
-              .build();
+      OkHttpClient client = getUnsafeOkHttpClient(timeout);
+//          new OkHttpClient.Builder()
+//              .connectTimeout((long) timeout, TimeUnit.SECONDS)
+//              .writeTimeout((long) timeout, TimeUnit.SECONDS)
+//              .readTimeout((long) timeout, TimeUnit.SECONDS)
+//              .build();
 
       call = client.newCall(request);
       Response response = call.execute();
@@ -325,6 +332,53 @@ public class UploadWorker extends ListenableWorker implements CountProgressListe
       return handleException(context, ex, "upload error");
     } finally {
       call = null;
+    }
+  }
+
+  private static OkHttpClient getUnsafeOkHttpClient(int timeout) {
+    try {
+      // Create a trust manager that does not validate certificate chains
+      final TrustManager[] trustAllCerts = new TrustManager[] {
+              new X509TrustManager() {
+                @Override
+                public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                }
+
+                @Override
+                public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                }
+
+                @Override
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                  return new java.security.cert.X509Certificate[]{};
+                }
+              }
+      };
+
+      // Install the all-trusting trust manager
+      final SSLContext sslContext = SSLContext.getInstance("SSL");
+      sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+      // Create an ssl socket factory with our all-trusting manager
+      final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+      OkHttpClient.Builder builder = new OkHttpClient.Builder();
+      builder.sslSocketFactory(sslSocketFactory, (X509TrustManager)trustAllCerts[0]);
+      builder.hostnameVerifier(new HostnameVerifier() {
+        @Override
+        public boolean verify(String hostname, SSLSession session) {
+          return true;
+        }
+      });
+
+      OkHttpClient okHttpClient = builder
+              .connectTimeout((long) timeout, TimeUnit.SECONDS)
+              .writeTimeout((long) timeout, TimeUnit.SECONDS)
+              .readTimeout((long) timeout, TimeUnit.SECONDS)
+              .build();
+
+      return okHttpClient;
+    } catch (Exception e) {
+      throw new RuntimeException(e);
     }
   }
 
